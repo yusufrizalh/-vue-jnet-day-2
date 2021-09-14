b166a578d07732a5e50c2915e487fb8d74184850
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-14 15:16:43 +0700

    modal-project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
+  <button @click.alt="toggleModal">open modal (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import components
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue App :)",
+      header: "Signup for Giveaway!",
+      text: "Grab your coupon for half price!",
+      showModal: false,
+    };
+  },
+  components: { Modal },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +43,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #e9e9e9;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
